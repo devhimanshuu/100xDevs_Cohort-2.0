fda29f959d281f9a8e3c7eea3e65d71b024f3b97
devhimanshuu <devhimanshuu@gmail.com>
2024-02-01 20:22:43 +0530

    code updated

--- 0-1_Cohort/Week-6/code/src/App.jsx
+++ 0-1_Cohort/Week-6/code/src/App.jsx
@@ -1,50 +1,43 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
-let counter = 4;
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "go to gym ",
-      description: "go to gym today",
-    },
-    {
-      id: 2,
-      title: "go to college",
-      description: "go to college today",
-    },
-    {
-      id: 3,
-      title: "go to cohort ",
-      description: "go to cohort today",
-    },
-  ]);
+// import { useState } from "react";
 
-  function addTodo() {
-    setTodos([
-      ...todos,
-      {
-        id: counter++,
-        title: Math.random(),
-        description: Math.random(),
-      },
-    ]);
-  }
+import { useState, useEffect } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    setInterval({}=> {
+      fetch("https://sum-server.100xdevs.com/todos").then(async function (res) {
+        const json = await res.json();
+        setTodos(json.todos);
+      })
+    }, 10000)
+  }),
+      []
+    }
+
   return (
     <div>
-      <button onClick={addTodo}>Add a todo</button>
+      {todos.length}
       {todos.map((todo) => (
-        <Todo key={todo.id} title={todo.title} description={todo.description} />
+        <Todo
+          key={todo.key}
+          title={todo.title}
+          description={todo.description}
+        />
       ))}
     </div>
   );
 }
-function Todo({ title, description }) {
+
+function Todo(title, description) {
   return (
     <div>
       <h1>{title}</h1>
-      <h5>{description}</h5>
+      <h4>{description}</h4>
     </div>
   );
 }
+
 export default App;
