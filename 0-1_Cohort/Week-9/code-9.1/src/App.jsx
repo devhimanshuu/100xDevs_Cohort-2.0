8c3f2b504241307bcaef9658ca0feeddf900a472
devhimanshuu <devhimanshuu@gmail.com>
2024-03-12 03:37:20 +0530

    code-9.1

---
+++ 0-1_Cohort/Week-9/code-9.1/src/App.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-undef */
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function useTodos(n) {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      });
+    }, n * 1000);
+
+    axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+      setTodos(res.data.todos);
+      setLoading(false);
+    });
+
+    return () => {
+      clearInterval(value);
+    };
+  }, [n]);
+  return { todos, loading };
+}
+
+function App() {
+  const todos = useTodos(5);
+
+  if (loading) {
+    return <div>loading......</div>;
+  }
+  return (
+    <>
+      {todos.map((todo) => (
+        <Track todo={todo} />
+      ))}
+    </>
+  );
+}
+
+function Track({ todo }) {
+  return (
+    <div>
+      {todo.title}
+      <br />
+      {todo.description}
+    </div>
+  );
+}
+
+export default App;
