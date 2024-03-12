3829d8696583ed870d310fd3955809820ff6e779
devhimanshuu <devhimanshuu@gmail.com>
2024-03-12 20:21:20 +0530

    useMousePointer

---
+++ 0-1_Cohort/Week-9/code-9.1/Hooks/useMousePointer.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useMousePointer = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return position;
+};
+
+function App() {
+  const mousePointer = useMousePointer();
+
+  return (
+    <>
+      Your mouse position is {mousePointer.x} {mousePointer.y}
+    </>
+  );
+}
+
+export default App;
