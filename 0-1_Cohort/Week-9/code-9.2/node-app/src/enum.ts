09ed5fdd6c29bf4b9604cb337e19fa5c9a11b5f3
devhimanshuu <devhimanshuu@gmail.com>
2024-03-14 13:44:38 +0530

    enums

---
+++ 0-1_Cohort/Week-9/code-9.2/node-app/src/enum.ts
@@ -0,0 +1,13 @@
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+function doSomething(keyPressed: Direction) {
+  // do something.
+}
+
+doSomething(Direction.Up);
+console.log(Direction.Up);
